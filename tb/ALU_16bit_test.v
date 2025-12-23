7feed2620d8b8f55e728f6b3fa11aa1468747db4
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-23 16:21:28 +0530

    Create ALU_16bit_test module
    Added testbench for 16-bit ALU functionality.

--- tb/ALU_16bit_test.v
+++ tb/ALU_16bit_test.v
@@ -1,1 +1,35 @@
-//testbench code
+`timescale 1ns / 1ps 
+module ALU_16bit_test; 
+//Inputs 
+ reg[15:0] A,B; 
+ reg[3:0] ALU_Sel; 
+ 
+//Outputs 
+ wire[15:0] ALU_Out; 
+ wire CarryOut; 
+ integer i; 
+  
+// Verilog code for ALU   
+ALU_16bit test_unit( 
+            A,B,  // ALU 8-bit Inputs                  
+            ALU_Sel,// ALU Selection 
+            ALU_Out, // ALU 8-bit Output 
+            CarryOut // Carry Out Flag 
+     ); 
+initial begin 
+  // hold reset state for 100 ns. 
+  A = 16'h0A; 
+  B = 4'h02; 
+  ALU_Sel = 4'h0; 
+       
+  for (i=0;i<=11;i=i+1) 
+      begin 
+       ALU_Sel = ALU_Sel + 16'h01; 
+       #10; 
+      end 
+       
+      A = 16'hF6; 
+      B = 16'h0A; 
+       
+    end 
+endmodule
