5b51124dab878b5f1e92521d658ec4d73811c670
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-23 16:24:49 +0530

    Enhance PS_test_ALU testbench functionality
    Updated the PS testbench for ALU with proper initialization and monitoring.

--- tb/PS_test_ALU.v
+++ tb/PS_test_ALU.v
@@ -1,1 +1,42 @@
-// PS testbench
+//`timescale 1ns / 1ps 
+//For Post Synthesis Timing Simulation with back-annotation 
+`include "osu05_stdcells.v" 
+`include "ALU_nbit_RTL.v" 
+ 
+module PS_test_ALU; 
+//Inputs 
+ reg[15:0] A,B; 
+ reg[3:0] ALU_Sel; 
+ 
+//Outputs 
+ wire[15:0] ALU_Out; 
+ wire CarryOut; 
+ // Verilog code for ALU 
+ integer i; 
+//ALU_16bit test_unit( 
+//            A,B,  // ALU 16-bit Inputs                  
+  //          ALU_Sel,// ALU Selection 
+    //        ALU_Out, // ALU 16-bit Output 
+      //      CarryOut // Carry Out Flag 
+     //); 
+  initial $sdf_annotate("ALU_16bit_RTL.sdf", PS_test_ALU, ,"sdf_ALU_16bit.log"); 
+ 
+    initial begin 
+    // hold reset state for 100 ns. 
+      A = 16'h0A; 
+      B = 4'h02; 
+      ALU_Sel = 4'h0;      
+      for (i=0;i<=11;i=i+1) 
+      begin 
+       ALU_Sel = ALU_Sel + 4'h01; 
+       #10; 
+      end 
+       
+      A = 16'hF6; 
+      B = 16'hFF; 
+       
+    end 
+ALU_16bit v_1(A,B,ALU_Sel,ALU_Out,CarryOut); 
+initial $monitor($time, "   A=%d, B=%d ALU_Sel=%d ALU_out=%d CarryOut = %d", A, B, 
+ALU_Sel, ALU_Out, CarryOut); 
+endmodule
